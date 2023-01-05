fcfe56621f762ed39b8ef82787a04a03e8561841
Gauri <2016.gauri.sagane@ves.ac.in>
2023-01-05 12:47:47 -0600

    Add code for Side Drawer

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { AppBar, Toolbar, IconButton } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import SideDrawer from './SideDrawer';
 
 
 const Header = () => {
+    const [drawerOpen, setDrawerOpen] = useState(false)
+    
+    const toggleDrawer = (value) => {
+        setDrawerOpen(value)
+    }
+
+
     return (
         <AppBar position='fixed' style ={{
             backgroundColor: '#2f2f2f',
@@ -17,10 +24,10 @@ const Header = () => {
                     <div className='header_logo_title'>Musical Events</div>
 
                 </div>
-                <IconButton aria-label='Menu' color='inherit'>
+                <IconButton aria-label='Menu' color='inherit' onClick={() => toggleDrawer(true)}>
                     <MenuIcon />
                 </IconButton>
-                <SideDrawer />
+                <SideDrawer open={drawerOpen} onClose={(value) => toggleDrawer(value)}/>
             </Toolbar>
         </AppBar>
     );
