9f1743284c254d8fc80d941d14c4bfcc2fda5d95
Gauri <2016.gauri.sagane@ves.ac.in>
2023-01-06 15:59:56 -0600

    Add Code for Venue Info and Highlights

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './resources/styles.css';
 import Header from './components/header_footer/Header';
 import Footer from './components/header_footer/Footer';
 import Featured from './components/featured/Featured';
+import VenueNfo from './components/venueNfo/VenueNfo';
+import Highlights from './components/highlights/Highlights';
 
 function App() {
   return (
@@ -9,6 +11,12 @@ function App() {
       <Header />
       <>
         <Featured />
+      </>
+      <>
+        <VenueNfo />
+      </>
+      <>
+        <Highlights />
       </>
       <div style={{backgroundColor: 'red', height:'800px'}}></div>
       <div style={{backgroundColor: 'blue', height:'800px'}}></div>
---
+++ src/components/highlights/Discount.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { Fade, Slide } from 'react-awesome-reveal';
+
+function Discount() {
+    const end = 30;
+    const[start, setStart] = useState(0);
+
+    const percentage = () => {
+        if(start < end){
+            setStart(prevCount => prevCount+1)
+        }
+    }
+
+    useEffect(() => {
+        if(start > 0 && start < 30){
+            setTimeout(() => {
+                setStart(prevCount => prevCount+1)
+            }, 30);
+        }
+    },[start])
+
+    return (
+        <div className='center_wrapper'>
+            <div className='discount_wrapper'>
+                <Fade onVisibilityChange={(inView) => {
+                    if(inView){
+                        percentage()
+                    }
+                }}>
+                    <div className='discount_percentage'>
+                        <span>{start}%</span>
+                        <span>OFF</span>
+                    </div>
+                </Fade>
+                <Slide right>
+                    <div className='discount_description'>
+                        <h3>Purchase tickets before 30th June</h3>
+                        <p>
+                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+                        </p>
+                    </div>
+                </Slide>
+            </div>
+        </div> 
+    );
+}
+
+export default Discount;
